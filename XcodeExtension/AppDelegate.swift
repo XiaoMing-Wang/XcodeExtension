a6ad759aff0ece95f9a462b5603f920ed3c6f73d
wq <wq>
2020-10-29 14:41:24 +0800

    Initial Commit

---
+++ XcodeExtension/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  XcodeExtension
+//
+//  Created by imMac on 2020/10/29.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ XcodeExtension/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  XcodeExtension
+//
+//  Created by imMac on 2020/10/29.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
