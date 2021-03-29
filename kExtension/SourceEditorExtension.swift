bfeac9c729d34c762652978547195ce07290e1d7
wq <wq>
2021-03-29 11:21:25 +0800

    no message

---
+++ kExtension/SourceEditorExtension.swift
@@ -0,0 +1,26 @@
+//
+//  SourceEditorExtension.swift
+//  kExtension
+//
+//  Created by imMac on 2020/10/29.
+//
+
+import Foundation
+import XcodeKit
+
+class SourceEditorExtension: NSObject, XCSourceEditorExtension {
+    
+    /*
+    func extensionDidFinishLaunching() {
+        // If your extension needs to do any work at launch, implement this optional method.
+    }
+    */
+    
+    /*
+    var commandDefinitions: [[XCSourceEditorCommandDefinitionKey: Any]] {
+        // If your extension needs to return a collection of command definitions that differs from those in its Info.plist, implement this optional property getter.
+        return []
+    }
+    */
+    
+}
